1523bda661110ddf0e74041ba98d60ef88343e8f
ahmadZaid <ahmaadzaid7@gmail.com>
2022-10-07 13:57:34 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        amber: "#FFB86C",
+        blueish: "#0E538C",
+        grayish: "#C0C0C0",
+      },
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"],
+        Pacifico: ["Pacifico", "cursive"],
+        Poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
